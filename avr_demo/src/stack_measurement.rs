e24ed054c9c5bf143d052326022e842614475ac8
Kaido Kert <kaidokert@gmail.com>
2026-07-17 18:20:45 -0700

    Use portable AVR stack adapter

--- avr_demo/src/stack_measurement.rs
+++ avr_demo/src/stack_measurement.rs
@@ -1,13 +1,10 @@
 use embedded_measure::stack::{Avr, LinkerStack, StackConfig};
 
-unsafe extern "C" {
-    static mut _end: u8;
-}
 // This harness is built and simulated for ATmega2560 by .cargo/config.toml.
 const RAMEND_EXCLUSIVE: usize = 0x2200;
 
 pub fn stack() -> LinkerStack<Avr> {
-    unsafe { LinkerStack::new(&raw mut _end, RAMEND_EXCLUSIVE as *mut u8, Avr) }
+    unsafe { LinkerStack::<Avr>::avr_runtime(RAMEND_EXCLUSIVE) }
 }
 
 pub const fn stack_config() -> StackConfig {
